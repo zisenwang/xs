940290553849e2c6ff0765e0018e9329d15e3024
zisenwang <xg3000wzzzz@gmail.com>
2025-03-09 09:24:25 +0800

    Add chat consultation button to the main page

---
+++ src/app/features/main-page/chat/chat.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {Button} from 'primeng/button';
+
+@Component({
+  selector: 'xs-main-page-chat',
+  imports: [
+    Button
+  ],
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.scss',
+  standalone: true
+})
+export class ChatComponent {
+
+}
