506d0158990b6c18a34522fbb37bb98f92656622
zisenwang <xg3000wzzzz@gmail.com>
2025-03-10 18:50:55 +0800

    correct amplify/storage/resource.ts syntax to perform prebuild on amplify

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,7 +4,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'staticStorage',
   access: allow => ({
-    '*': [
+    '/*': [
       allow.guest.to(['read']),
       allow.authenticated.to(['read', 'write', 'delete']),
     ],
