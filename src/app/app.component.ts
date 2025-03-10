c96c2aec221d9b3305461e7aedce5ab2709cdf4d
zisenwang <xg3000wzzzz@gmail.com>
2025-03-10 17:59:38 +0800

    Refactor scroll service and update Husky hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent} from '@app/features/header/header.component';
-import { BannerComponent} from './features/main-page/banner/banner.component';
-import { ServicesComponent} from './features/main-page/services/services.component';
-import { FooterComponent} from './features/main-page/footer/footer.component';
 import {MainPageComponent} from './features/main-page/main-page.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'xs-root',
   imports: [ MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
