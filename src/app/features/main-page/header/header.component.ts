ce0543f2bb978823a25e32b378c4388b8e7796f6
zisenwang <xg3000wzzzz@gmail.com>
2025-04-01 10:01:55 +0800

    Simplify header component by removing dynamic translations.
    Replaced dynamically translated menu items with static ones to reduce complexity. Updated `p-menubar` to bind directly to the static item model. Cleaned unused imports and removed OnInit implementation for a more streamlined component.

--- src/app/features/main-page/header/header.component.ts
+++ src/app/features/main-page/header/header.component.ts
@@ -1,6 +1,5 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { MenuItem } from 'primeng/api';
 import { Menubar } from 'primeng/menubar';
 import { ScrollService } from '@services/index';
 import { CommonModule } from '@angular/common';
@@ -13,57 +12,41 @@ import {TranslateModule, TranslateService} from '@ngx-translate/core';
   styleUrl: './header.component.scss',
   standalone: true,
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent {
   private scrollService = inject(ScrollService);
   private translate = inject(TranslateService);
 
   currentLang = this.translate.currentLang;
 
-  items: MenuItem[] = [];
-
-  ngOnInit() {
-    this.translateMenuItems();
-  }
+  items = [
+    {
+      label: 'header.aboutUs',
+      icon: 'pi pi-fw pi-briefcase',
+      command: () => this.scrollService.scrollToElement('intro'),
+    },
+    {
+      label: 'header.teachers',
+      icon: 'pi pi-fw pi-user',
+      command: () => this.scrollService.scrollToElement('teacher'),
+    },
+    {
+      label: 'header.programs',
+      icon: 'pi pi-fw pi-cloud',
+    },
+    {
+      label: 'header.successStories',
+      icon: 'pi pi-fw pi-comments',
+      command: () => this.scrollService.scrollToElement('testimonials'),
+    },
+    {
+      label: 'header.contactUs',
+      icon: 'pi pi-fw pi-envelope',
+      command: () => this.scrollService.scrollToElement('contact'),
+    },
+  ];
 
   selectLang(lang: string) {
     this.translate.use(lang);
     this.currentLang = lang;
-  }
-
-  private translateMenuItems() {
-    this.translate.get([
-      'header.aboutUs',
-      'header.teachers',
-      'header.programs',
-      'header.successStories',
-      'header.contactUs'
-    ]).subscribe(translations => {
-      this.items = [
-        {
-          label: translations['header.aboutUs'],
-          icon: 'pi pi-fw pi-briefcase',
-          command: () => this.scrollService.scrollToElement('intro'),
-        },
-        {
-          label: translations['header.teachers'],
-          icon: 'pi pi-fw pi-user',
-          command: () => this.scrollService.scrollToElement('teacher'),
-        },
-        {
-          label: translations['header.programs'],
-          icon: 'pi pi-fw pi-cloud',
-        },
-        {
-          label: translations['header.successStories'],
-          icon: 'pi pi-fw pi-comments',
-          command: () => this.scrollService.scrollToElement('testimonials'),
-        },
-        {
-          label: translations['header.contactUs'],
-          icon: 'pi pi-fw pi-envelope',
-          command: () => this.scrollService.scrollToElement('contact'),
-        },
-      ];
-    });
   }
 }
