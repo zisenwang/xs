fde2a8880e313161fbfea3e9a0e109df2666ca30
zisenwang <xg3000wzzzz@gmail.com>
2025-04-27 10:51:14 +0800

    Refactor Contact Us form with PrimeNG components
    Replaced native form inputs with PrimeNG components for improved UI/UX. Added form validation with error message handling, and integrated PrimeNG Toast for user feedback on submission. Updated data structure and logic to align with new form controls.

--- src/app/features/contact-us/contact-us.component.ts
+++ src/app/features/contact-us/contact-us.component.ts
@@ -1,63 +1,71 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { BgTitleComponent } from '@shared/components/bg-title/bg-title.component';
 import { BG_CONTACT_US_URL, CONTACT_US_TITLE } from '@shared/constants';
 import { TranslatePipe } from '@ngx-translate/core';
 import { generateClient} from 'aws-amplify/api'
 import type {Schema} from "../../../../amplify/data/resource"
+import {Button} from 'primeng/button';
+import {Toast} from 'primeng/toast';
+import {InputText} from 'primeng/inputtext';
+import {Textarea} from 'primeng/textarea';
+import {MessageService} from 'primeng/api';
+import {Message} from 'primeng/message';
+import {NgClass} from '@angular/common';
+import {Checkbox} from 'primeng/checkbox';
 
 @Component({
   selector: 'xs-contact-us',
-  imports: [ReactiveFormsModule, BgTitleComponent, TranslatePipe],
+  imports: [
+    ReactiveFormsModule,
+    BgTitleComponent,
+    TranslatePipe,
+    Button, Toast, InputText, Textarea, Message, NgClass, Checkbox],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss',
+  providers: [MessageService]
 })
 export class ContactUsComponent {
   protected readonly title = CONTACT_US_TITLE;
   protected readonly bgUrl = BG_CONTACT_US_URL;
+
+  private messageService = inject(MessageService);
   contactForm: FormGroup;
-  client = generateClient<Schema>()
+  client = generateClient<Schema>();
+
 
   tuitionOptions = ['物理工程', '数学', '生物', '化学', '经济'];
 
+
   constructor(private fb: FormBuilder) {
     this.contactForm = this.fb.group({
-      firstName: [''],
-      lastName: [''],
-      tuitionFor: [''],
-      trial: [''],
-      email: [''],
+      name: ['', Validators.required],
+      subjects: [''],
+      contactDetail: ['', Validators.required],
       message: [''],
     });
   }
 
   async onSubmit() {
-    try {
-      const {data, errors} = await this.client.queries.sendEmail({
-        params:{
-          firstName: 'jason',
-          lastName: 'wong',
-          email: 'example@example.com',
-          subjects: JSON.stringify(['maths','physics']),
-          message: 'hello world'
-        }
-      })
-
-      console.log(data, errors)
-
-    //   const response = await post({
-    //     apiName: 'my-first-function',
-    //     path:'/'
-    //   }).response;
-    //   console.log(response);
-    } catch (error) {
-      console.log(error);
+    if (this.contactForm.invalid) {
+      console.log(this.contactForm)
+      this.messageService.add({severity:'error', summary:'Failed', detail:'Some fields are missing'});
+      return;
     }
-    // if (this.contactForm.valid) {
-    //   console.log(this.contactForm.value);
-    //   alert('Thank you! Well contact you soon.');
-    // } else {
-    //   alert('Please complete the required fields.');
+    this.messageService.add({severity:'success', summary:'Success', detail:'Your message has been sent, we will contact you soon'});
+    // try {
+    //   const {data, errors} = await this.client.queries.sendEmail({
+    //     params:{
+    //       firstName: 'jason',
+    //       lastName: 'wong',
+    //       email: 'example@example.com',
+    //       subjects: JSON.stringify(['maths','physics']),
+    //       message: 'hello world'
+    //     }
+    //   })
+    //   console.log(data, errors)
+    // } catch (error) {
+    //   console.log(error);
     // }
   }
 }
