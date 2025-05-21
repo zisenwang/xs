df884024a4e2185c4a0e9dc40d2c67d5107e93d5
zisenwang <xg3000wzzzz@gmail.com>
2025-05-22 04:51:59 +0800

    Refactor "About Us" page to use dynamic feature cards

--- src/app/features/about-us/about-us.component.ts
+++ src/app/features/about-us/about-us.component.ts
@@ -6,10 +6,12 @@ import {
 } from '@shared/constants';
 import { BgTitleComponent } from '@shared/components/bg-title/bg-title.component';
 import { TranslatePipe } from '@ngx-translate/core';
+import {Card} from 'primeng/card';
+import {FEATURES} from '@app/features/about-us/about-us.constants';
 
 @Component({
   selector: 'xs-about-us',
-  imports: [BgTitleComponent, TranslatePipe],
+  imports: [BgTitleComponent, TranslatePipe, Card],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss',
 })
@@ -17,4 +19,5 @@ export class AboutUsComponent {
   protected readonly bgUrl = BG_ABOUT_US_URL;
   protected readonly title = COMPANY_FULL_NAME;
   protected readonly subtitle = ABOUT_US_SUBTITLE;
+  protected readonly features = FEATURES;
 }
