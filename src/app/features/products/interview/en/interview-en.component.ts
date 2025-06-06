43ef34e8001339eabd0eb5c8df3d1ed94284df1d
zisenwang <xg3000wzzzz@gmail.com>
2025-06-06 13:50:40 +0800

    Add language-specific components for interview section

---
+++ src/app/features/products/interview/en/interview-en.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'xs-products-interview-en',
+  imports: [],
+  templateUrl: './interview-en.component.html',
+  styleUrl: './interview-en.component.scss',
+})
+export class InterviewEnComponent {
+}
--- src/app/features/products/products.routes.ts
+++ src/app/features/products/products.routes.ts
@@ -2,9 +2,9 @@ import {Routes} from '@angular/router';
 import {ProductsComponent} from '@app/features/products/products.component';
 import {FoundationComponent} from '@app/features/products/foundation/foundation.component';
 import {AdvancedComponent} from '@app/features/products/advanced/advanced.component';
-import {InterviewComponent} from '@app/features/products/interview/interview.component';
 import {PrepComponent} from '@app/features/products/prep/prep.component';
 import {SprintComponent} from '@app/features/products/sprint/sprint.component';
+import {InterviewComponent} from '@app/features/products/interview/interview.component';
 
 export const productsRoutes: Routes = [
   {
