0a8c0f5f79969cf22d4a61f6b6fc3d2e6eea6cf0
zisenwang <xg3000wzzzz@gmail.com>
2025-06-11 13:16:59 +0800

    Fix code formatting and enhance readability.

--- src/app/features/products/interview/interview.component.ts
+++ src/app/features/products/interview/interview.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { LANG_EN, LANG_ZH_CN } from '@shared/constants';
 import { InterviewEnComponent } from '@app/features/products/interview/en/interview-en.component';
@@ -14,13 +14,14 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 })
 export class InterviewComponent implements OnInit {
   private translate = inject(TranslateService);
+  private readonly destroyRef = inject(DestroyRef);
   currentLang =
     this.translate.currentLang || this.translate.getDefaultLang() || LANG_ZH_CN;
   protected readonly LANG_EN = LANG_EN;
   protected readonly LANG_ZH_CN = LANG_ZH_CN;
 
   ngOnInit() {
-    this.translate.onLangChange.pipe(takeUntilDestroyed()).subscribe(event => {
+    this.translate.onLangChange.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(event => {
       this.currentLang = event.lang;
     });
   }
