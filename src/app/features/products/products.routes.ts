70cbf6471f644aa953b4c11160690037b402f466
zisenwang <xg3000wzzzz@gmail.com>
2025-07-28 17:13:54 +0800

    remove unnecessary code

--- src/app/features/products/products.routes.ts
+++ src/app/features/products/products.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from '@app/features/products/products.component';
-import { FoundationComponent } from '@app/features/products/foundation/foundation.component';
 import { AdvancedComponent } from '@app/features/products/advanced/advanced.component';
-import { PrepComponent } from '@app/features/products/prep/prep.component';
 import { SprintComponent } from '@app/features/products/sprint/sprint.component';
 import { InterviewComponent } from '@app/features/products/interview/interview.component';
 
